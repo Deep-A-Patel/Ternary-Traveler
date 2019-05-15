223c58142cc4406c223aa8739bf7934d3a1ecf9e
Deep Patel <deep.ag123007@gmail.com>
2019-05-15 15:38:06 -0500

    paragraph 1 is done

--- src/scripts/dbCalls.js
+++ src/scripts/dbCalls.js
@@ -1,10 +1,29 @@
 const baseURL = "http://0.0.0.0:8088";
 
-const APIManager = {
-    getAllPlaces: function() {
-        return fetch(`${baseURL}/places`)
-                .then(response => response.json())
-    }
-}
+const API = {
+  getInterests: function() {
+    return fetch(`${baseURL}/interests?_expand=place`).then(response =>
+      response.json()
+    );
+  },
+  postInterests: function(obj) {
+    return fetch(`${baseURL}/interests?_expand=place`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(obj)
+    }).then(response => response.json());
+  },
+  patchInterests: function(obj, id) {
+    return fetch(`${baseURL}/interests?_expand=place`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(obj)
+    }).then(response => response.json());
+  }
+};
 
-export default APIManager;
+export default API;
